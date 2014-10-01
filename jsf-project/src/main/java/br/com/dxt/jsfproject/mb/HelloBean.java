e73dfb7d696ade6d3897c9b5c105f3cff83a0a0d
Walter Mesquita <wapmesquita@gmail.com>
2014-09-30 21:20:54 -0300

    Acessando atributos de classe em xhtml

--- jsf-project/src/main/java/br/com/dxt/jsfproject/mb/HelloBean.java
+++ jsf-project/src/main/java/br/com/dxt/jsfproject/mb/HelloBean.java
@@ -5,18 +5,22 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.dxt.jsfproject.domain.Pessoa;
+
 @ManagedBean
 @SessionScoped
 public class HelloBean implements Serializable {
-	 
+
 	private static final long serialVersionUID = 1L;
- 
-	private String name;
- 
-	public String getName() {
-		return name;
+
+	private Pessoa pessoa = new Pessoa();
+
+	public Pessoa getPessoa() {
+		return pessoa;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
+
 }
