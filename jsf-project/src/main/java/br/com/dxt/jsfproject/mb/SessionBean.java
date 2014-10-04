eb06cf3e8a769512d63a6372641872a9538952f7
Walter Mesquita <wapmesquita@gmail.com>
2014-10-04 15:22:19 -0300

    Exemplos de scopes @Request e @Session

---
+++ jsf-project/src/main/java/br/com/dxt/jsfproject/mb/SessionBean.java
@@ -0,0 +1,31 @@
+package br.com.dxt.jsfproject.mb;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.dxt.jsfproject.domain.Categoria;
+import br.com.dxt.jsfproject.service.CategoriaService;
+
+@ManagedBean
+@SessionScoped
+public class SessionBean implements Serializable{
+
+	private static final long serialVersionUID = -4393817636057275303L;
+	private Categoria categoria;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Collection<Categoria> getListCategoria() {
+		return CategoriaService.findAll();
+	}
+
+}
