e5f045bf09a68fdc84148a3000521907d6d96e54
Walter Mesquita <wapmesquita@gmail.com>
2014-10-04 15:06:27 -0300

    ValueChangeListener e p:collector

--- jsf-project/src/main/java/br/com/dxt/jsfproject/mb/SelectBean.java
+++ jsf-project/src/main/java/br/com/dxt/jsfproject/mb/SelectBean.java
@@ -1,18 +1,27 @@
 package br.com.dxt.jsfproject.mb;
 
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ValueChangeEvent;
 
 import br.com.dxt.jsfproject.domain.Categoria;
 import br.com.dxt.jsfproject.service.CategoriaService;
 
 @ManagedBean
 @SessionScoped
-public class SelectBean {
+public class SelectBean implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8135048491106322697L;
 	private Categoria categoria;
+	private static Set<Categoria> selecteds;
 
 	public Categoria getCategoria() {
 		return categoria;
@@ -24,5 +33,21 @@ public class SelectBean {
 
 	public Collection<Categoria> getListCategoria() {
 		return CategoriaService.findAll();
+	}
+
+	public Set<Categoria> getSelecteds() {
+		if (selecteds == null) {
+			selecteds = new HashSet<Categoria>();
+		}
+		return selecteds;
+	}
+
+	public void onChangeCategoria(ValueChangeEvent event) {
+		if (event.getNewValue() != null) {
+			System.out.println("New Value: " + ((Categoria) event.getNewValue()).getNome());
+			Integer id = ((Categoria) event.getNewValue()).getId();
+			Categoria findById = CategoriaService.findById(id);
+			getSelecteds().add(findById);
+		}
 	}
 }
