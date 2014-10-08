8692f2384543d6bda7879db7b538752fbadbd958
Walter Mesquita <wapmesquita@gmail.com>
2014-10-07 21:39:46 -0300

    Hashcode and equals

--- jsf-project/src/main/java/br/com/dxt/jsfproject/domain/Categoria.java
+++ jsf-project/src/main/java/br/com/dxt/jsfproject/domain/Categoria.java
@@ -21,4 +21,35 @@ public class Categoria {
 		this.nome = nome;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
 }
